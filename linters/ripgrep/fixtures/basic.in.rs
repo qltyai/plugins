fb7682fca4ab12f4e1e2f6e00f5449a402632147
marschattha <arslan_21@rocketmail.com>
2024-05-03 17:55:35 +0500

    PR feedback

--- linters/ripgrep/fixtures/basic.in.rs
+++ linters/ripgrep/fixtures/basic.in.rs
@@ -4,5 +4,6 @@ fn main() {
     // HACK
     let note = 1; // doesn't capture lowercase note
     println!("note: {}", note);
+    // FIXME: This test fails on the first day of each month
     println!("Hello, world!");
 }
