546a93ae74c83312adeffbbbe41d7e2169f59432
Loren Segal <lsegal@soen.ca>
2024-07-19 23:55:46 -0700

    Try a thing

--- jest.config.ts
+++ jest.config.ts
@@ -4,9 +4,6 @@ const config: Config = {
   preset: "ts-jest",
   modulePaths: ["<rootDir>"],
   setupFilesAfterEnv: ["<rootDir>/tests/jest_setup.ts"],
-  reporters: process.env.CI
-    ? [["github-actions", { silent: false }], "summary"]
-    : ["default"],
   modulePathIgnorePatterns: ["<rootDir>/.qlty/"],
 };
 
