2a100b36ef774de3155798b5cf969da1fe36729a
marschattha <arslan_21@rocketmail.com>
2024-08-05 20:49:31 +0500

    Test swiftlint on linux

--- linters/swiftlint/swiftlint.test.ts
+++ linters/swiftlint/swiftlint.test.ts
@@ -1,10 +1,3 @@
 import { linterCheckTest } from "tests";
 
-// only runs on macOS
-if (process.platform === "darwin") {
-  linterCheckTest("swiftlint", __dirname);
-} else {
-  test("Skipping swiftlint tests on non-macOS platform", () => {
-    expect(true).toBe(true);
-  });
-}
+linterCheckTest("swiftlint", __dirname);
