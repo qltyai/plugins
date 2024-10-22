102603f49203869412c1b23f99209728f64b1541
marschattha <arslan_21@rocketmail.com>
2024-10-22 21:02:52 +0500

    Reduce try catch throw pattern for runtimes

--- scripts/fetchLatestVersion/node.ts
+++ scripts/fetchLatestVersion/node.ts
@@ -5,13 +5,8 @@ export async function fetchLatestVersionForNode(
 ): Promise<string> {
   const cmd = `npm view ${nodePackage} version`;
 
-  try {
-    const output: string = execSync(cmd, { encoding: 'utf8' });
-    const versionString = output.trim();
+  const output: string = execSync(cmd, { encoding: "utf8" });
+  const versionString = output.trim();
 
-    return versionString;
-  } catch (error) {
-    console.error(`Error fetching latest version: ${error}`);
-    throw new Error(`Failed to fetch latest version for ${nodePackage}`);
-  }
+  return versionString;
 }
--- scripts/fetchLatestVersion/ruby.ts
+++ scripts/fetchLatestVersion/ruby.ts
@@ -2,25 +2,18 @@ import { execSync } from "child_process";
 
 export async function fetchLatestVersionForRuby(gem: string): Promise<string> {
   const cmd = `ruby -S gem search '^${gem}$'`;
+  const output: string = execSync(cmd, { encoding: "utf8" });
 
-  try {
-    const output: string = execSync(cmd, { encoding: 'utf8' });
+  const versionString = output
+    .split("\n")
+    .find((line) => line.startsWith(gem))
+    ?.split(/\s+/)
+    .pop();
 
-    const versionString = output
-      .split("\n")
-      .find((line) => line.startsWith(gem))
-      ?.split(/\s+/)
-      .pop();
-
-    if (!versionString) {
-      throw new Error(`Failed to find latest version for ${gem}`);
-    }
-
-    const version = versionString.replace(/^\(/, "").replace(/\)$/, "");
-
-    return version;
-  } catch (error) {
-    console.error(`Error fetching latest version: ${error}`);
-    throw new Error(`Failed to fetch latest version for ${gem}`);
+  if (!versionString) {
+    throw new Error(`Failed to find latest version for ${gem}`);
   }
+
+  const version = versionString.replace(/^\(/, "").replace(/\)$/, "");
+  return version;
 }
